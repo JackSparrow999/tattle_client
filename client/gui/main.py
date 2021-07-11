ef09f101538caeb974bde58a4e5f440bfbd911e8
ronaq <ronaq.behura@acko.tech>
2021-07-11 16:54:01 +0530

    refactor

--- client/gui/main.py
+++ client/gui/main.py
@@ -8,37 +8,42 @@ class MainApp(App):
 
         super_box = BoxLayout(orientation = 'vertical')
 
+        #info box
         info_box = BoxLayout(orientation='horizontal')
-        type_box = BoxLayout(orientation='horizontal')
 
         chats = BoxLayout(orientation='horizontal')
         online_status = BoxLayout(orientation='horizontal')
-
-        info_box.add_widget(chats)
-        info_box.add_widget(online_status)
-
-        super_box.add_widget(info_box)
-        super_box.add_widget(type_box)
-
-        text_input = TextInput(text='', multiline=False, text_validate_unfocus=False)
-        text_input.bind(on_text_validate=self.on_enter_in_chat)
-
         chat_label = Label(text='Chat is here',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-
-        online_label = Label(text='online is here',
                            size_hint=(.5, .5),
                            pos_hint={'center_x': .5, 'center_y': .5})
+        online_label = Label(text='online is here',
+                             size_hint=(.5, .5),
+                             pos_hint={'center_x': .5, 'center_y': .5})
+        chats.add_widget(chat_label)
+        online_status.add_widget(online_label)
+        info_box.add_widget(chats)
+        info_box.add_widget(online_status)
+        super_box.add_widget(info_box)
 
+
+        #text box
+        type_box = BoxLayout(orientation='horizontal')
+        text_input = TextInput(text='', multiline=False, text_validate_unfocus=False)
+        text_input.bind(on_text_validate=self.on_enter_in_chat)
         type_label = Label(text='Type something: ',
                            size_hint=(.5, .5),
                            pos_hint={'center_x': .5, 'center_y': .5})
-
         type_box.add_widget(text_input)
-        chats.add_widget(chat_label)
-        online_status.add_widget(online_label)
         type_box.add_widget(type_label)
+        super_box.add_widget(type_box)
+
+
+
+
+
+
+
+
 
         return super_box
 
